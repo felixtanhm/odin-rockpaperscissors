7d8c7c64a8df25a4059bfa07676de6595176865e
felixtanhm <felixtanhm@gmail.com>
2023-09-15 19:28:26 +0800

    Refactor | Include click handler

--- script.js
+++ script.js
@@ -10,20 +10,13 @@ let playerScore = 0;
 let comScore = 0;
 let gameRounds = 3;
 
-document.querySelectorAll("button").forEach((button) => {
-  button.addEventListener(
-    "click",
-    gamePlay(button.textContent.toLowerCase(), computerPlay())
-  );
-});
-
 // Determine who wins
 // Update DOM to reflect new score
 // Update rounds to reflect that a round has been played
 // When rounds reach 0, declare winner or draw
 
 //Define player choices
-let computerPlay = () => {
+const computerPlay = () => {
   let int = Math.floor(Math.random() * 3);
   return optionsArr[int];
 };
@@ -31,16 +24,24 @@ let computerPlay = () => {
 //Evaluate gameplay
 const gamePlay = (p1, p2) => {
   if (p1 == p2) {
-    return "It's a draw!";
+    console.log("It's a draw!");
   } else {
     let winner = options[p1] == p2 ? "Player" : "Computer";
     winner == "Player" ? playerScore++ : comScore++;
-    return `${winner} has won!`;
+    return console.log(`${winner} has won!`);
   }
 };
 
-//Execute gameplay
-for (let round = 0; round < gameRounds; round++) {
-  alert(gamePlay(playerPlay(), computerPlay()));
-  alert(`Player Score ${playerScore} | Computer Score ${comScore}`);
-}
+const handleClick = (e) => {
+  gamePlay(e.target.textContent.toLowerCase(), computerPlay());
+};
+
+// //Execute gameplay
+// for (let round = 0; round < gameRounds; round++) {
+//   alert(gamePlay(playerPlay(), computerPlay()));
+//   alert(`Player Score ${playerScore} | Computer Score ${comScore}`);
+// }
+
+document.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", handleClick);
+});
